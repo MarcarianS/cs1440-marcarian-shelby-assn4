6d742f6d3c70d4fac0bfb1cc016eccc514dbb343
Shelby Marcarian <stmarcarian@aggiemail.usu.edu>
2021-03-30 18:06:16 -0600

    Changed win grad and white to not be globals in julia. from hin julia

--- src/julia_fractal.py
+++ src/julia_fractal.py
@@ -6,25 +6,54 @@ import sys
 from tkinter import Tk, Canvas, PhotoImage, mainloop
 from time import time
 
+# Changes Made:
+# Get all the globals in one place, see if that changes anything
+# make GRAD into a named constant (temporary, won't be in this file forever)
+# make window a local variable (renamed from win for clarity)
+# Renamed b4 to before
+# remove the second photo.write
 
-def getColorFromPalette(z):
+
+# This is the color palette, which defines the palette that images are drawn
+# in as well as limiting the number of iterations the escape-time algorithm uses
+#
+# TODO: It would be nice to add more or different colors to this list, but it's
+# just so much work to calculate all of the in-between shades!
+GRAD = [
+        '#ffe4b5', '#ffe5b2', '#ffe7ae', '#ffe9ab', '#ffeaa8', '#ffeda4',
+        '#ffefa1', '#fff29e', '#fff49a', '#fff797', '#fffb94', '#fffe90',
+        '#fcff8d', '#f8ff8a', '#f4ff86', '#f0ff83', '#ebff80', '#e7ff7c',
+        '#e2ff79', '#ddff76', '#d7ff72', '#d2ff6f', '#ccff6c', '#c6ff68',
+        '#bfff65', '#b9ff62', '#b2ff5e', '#abff5b', '#a4ff58', '#9dff54',
+        '#95ff51', '#8dff4e', '#85ff4a', '#7dff47', '#75ff44', '#6cff40',
+        '#63ff3d', '#5aff3a', '#51ff36', '#47ff33', '#3eff30', '#34ff2c',
+        '#2aff29', '#26ff2c', '#22ff30', '#1fff34', '#1cff38', '#18ff3d',
+        '#15ff42', '#11ff47', '#0eff4c', '#0bff51', '#07ff57', '#04ff5d',
+        '#01ff63', '#00fc69', '#00f970', '#00f677', '#00f27d', '#00ef83',
+        '#00ec89', '#00e88e', '#00e594', '#00e299', '#00de9e', '#00dba3',
+        '#00d8a7', '#00d4ab', '#00d1af', '#00ceb3', '#00cab7', '#00c7ba',
+        '#00c4be', '#00c0c0', '#00b7bd', '#00adba', '#00a4b6', '#009cb3',
+        '#0093b0', '#008bac', '#0082a9', '#007ba6', '#0073a2', '#006b9f',
+        '#00649c', '#005d98', '#005695', '#004f92', '#00498e', '#00438b',
+        '#003d88', '#003784', '#003181', '#002c7e', '#00277a', '#002277',
+        ]
+WHITE = '#ffffff'
+
+global photo
+
+def getColorFromPalette(z, GRAD):
     """Return the index of the color of the current pixel within the Julia set
     in the palette array"""
 
     # c is the Julia Constant; varying this value can yield interesting images
     c = complex(-1.0, 0.0)
 
-    # I feel bad about all of the global variables I'm using.
-    # There must be a better way...
-    global grad
-    global win
-
     # loop through each color in the palette
-    for i in range(len(grad) - 1):
+    for i in range(len(GRAD) - 1):
         z = z * z + c  # Iteratively compute z1, z2, z3 ...
         if abs(z) > 2:
-            return grad[i]  # The sequence is unbounded
-    return grad[77]         # Else this is a bounded sequence
+            return GRAD[i]  # The sequence is unbounded
+    return GRAD[77]         # Else this is a bounded sequence
 
 
 
@@ -46,13 +75,11 @@ def getFractalConfigurationDataFromFractalRepositoryDictionary(dictionary, name)
 
 photo = None
 
-def makePicture(f, i, e):
+def makePicture(f, i, e, window):
     """Paint a Fractal image into the TKinter PhotoImage canvas.
     Assumes the image is 512x512 pixels."""
 
-    global win
-    global grad
-    global photo
+
 
     # Correlate the boundaries of the PhotoImage object to the complex
     # coordinates of the imaginary plane
@@ -62,9 +89,9 @@ def makePicture(f, i, e):
     max = ((f['centerX'] + (f['axisLength'] / 2.0)),
            (f['centerY'] + (f['axisLength'] / 2.0)))
 
-    global WHITE
+
     # Display the image on the screen
-    canvas = Canvas(win, width=512, height=512, bg=WHITE)
+    canvas = Canvas(window, width=512, height=512, bg=WHITE)
     canvas.pack()
     # TODO: Sometimes I wonder whether some of my functions are trying to do
     #       too many different things... this is the correct part of the
@@ -87,34 +114,12 @@ def makePicture(f, i, e):
         for c in range(512):
             x = min[0] + c * size
             y = min[1] + r * size
-            c2 = getColorFromPalette(complex(x, y))
+            c2 = getColorFromPalette(complex(x, y), GRAD)
             photo.put(c2, (c, 512 - r))
-        win.update()  # display a row of pixels
+        window.update()  # display a row of pixels
 
 
-# This is the color palette, which defines the palette that images are drawn
-# in as well as limiting the number of iterations the escape-time algorithm uses
-#
-# TODO: It would be nice to add more or different colors to this list, but it's
-# just so much work to calculate all of the in-between shades!
-grad = [
-        '#ffe4b5', '#ffe5b2', '#ffe7ae', '#ffe9ab', '#ffeaa8', '#ffeda4',
-        '#ffefa1', '#fff29e', '#fff49a', '#fff797', '#fffb94', '#fffe90',
-        '#fcff8d', '#f8ff8a', '#f4ff86', '#f0ff83', '#ebff80', '#e7ff7c',
-        '#e2ff79', '#ddff76', '#d7ff72', '#d2ff6f', '#ccff6c', '#c6ff68',
-        '#bfff65', '#b9ff62', '#b2ff5e', '#abff5b', '#a4ff58', '#9dff54',
-        '#95ff51', '#8dff4e', '#85ff4a', '#7dff47', '#75ff44', '#6cff40',
-        '#63ff3d', '#5aff3a', '#51ff36', '#47ff33', '#3eff30', '#34ff2c',
-        '#2aff29', '#26ff2c', '#22ff30', '#1fff34', '#1cff38', '#18ff3d',
-        '#15ff42', '#11ff47', '#0eff4c', '#0bff51', '#07ff57', '#04ff5d',
-        '#01ff63', '#00fc69', '#00f970', '#00f677', '#00f27d', '#00ef83',
-        '#00ec89', '#00e88e', '#00e594', '#00e299', '#00de9e', '#00dba3',
-        '#00d8a7', '#00d4ab', '#00d1af', '#00ceb3', '#00cab7', '#00c7ba',
-        '#00c4be', '#00c0c0', '#00b7bd', '#00adba', '#00a4b6', '#009cb3',
-        '#0093b0', '#008bac', '#0082a9', '#007ba6', '#0073a2', '#006b9f',
-        '#00649c', '#005d98', '#005695', '#004f92', '#00498e', '#00438b',
-        '#003d88', '#003784', '#003181', '#002c7e', '#00277a', '#002277',
-        ]
+
 
 
 # This dictionary contains the different views of the Julia set you can make
@@ -148,26 +153,24 @@ f = {
         }
 
 
-WHITE = '#ffffff'
 
 
 def julia_main(i):
 
     global photo
-    global win
+
     # Set up the GUI so that we can display the fractal image on the screen
-    b4 = time()
-    win = Tk()
+    before = time()
+    window = Tk()
 
     photo = PhotoImage(width=512, height=512)
-    makePicture(f[i], i, ".png")
+    makePicture(f[i], i, ".png", window)
 
-    print(f"Done in {time() - b4:.3f} seconds!", file=sys.stderr)
+    print(f"Done in {time() - before:.3f} seconds!", file=sys.stderr)
     # Output the Fractal into a .png image
     photo.write(i + ".png")
     print("Wrote picture " + i + ".png")
-    photo.write(i + ".png")
-
+    
     print("Close the image window to exit the program")
     # Call tkinter.mainloop so the GUI remains open
     mainloop()
